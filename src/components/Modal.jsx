2eb098f1393815ab5e2b111ed74f7623ea881c95
suportdevs <suportdevs@gmail.com>
2024-02-28 21:09:06 +0600

    working on modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,10 @@
 const Modal = () => {
     return (
-        <div></div>
+        <div className="fixed inset-0 w-screen h-screen backdrop-blur-sm">
+            <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+                <div className="bg-white p-5 rounded border shadow-2xl">modal</div>
+            </div>
+        </div>
     )
 }
 
