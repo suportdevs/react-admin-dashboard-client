155ea33a6aa107f35df77f8c9c3d4bd1f8d15a05
suportdevs <suportdevs@gmail.com>
2024-03-03 22:58:19 +0600

    designed login and register page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 import logo from "../images/logo.png"
 
-const Login = () => {
+const Register = () => {
     return (
         <div className="flex flex-col justify-center h-screen">
                 <div className="mx-auto">
                     <img src={logo} alt="Logo" className="mx-auto h-22 w-auto" />
-                    <h1 className="mt-10 text-2xl font-extrabold">Sign in to your account</h1>
+                    <h1 className="mt-10 text-2xl font-extrabold">Sign up to your account</h1>
                 </div>
                 <div className="mt-10 mx-auto w-96">
                     <form className="space-y-6">
+                        <div>
+                            <label htmlFor="username">Username</label>
+                            <div className="mt-2">
+                                <input type="text" name="username" placeholder="Username" autoFocus autoComplete="username" className="block w-full px-2 py-2 outline-none rounded-md shadow-sm border border-gray-300 focus:border-blue-400 focus:ring focus:ring-2 focus:ring-blue-200 " required/>
+                            </div>
+                        </div>
                         <div>
                             <label htmlFor="email">Email address</label>
                             <div className="mt-2">
@@ -28,15 +34,15 @@ const Login = () => {
                             </div>
                         </div>
                         <div>
-                            <button type="submit" className="block p-2 w-full rounded-md outline-none ring-2 ring-inset bg-indigo-600 text-white leading-6 hover:bg-indigo-500 focus:border-blue-700 focus:ring-2 focus:ring-blue-300 ">Sign In</button>
+                            <button type="submit" className="block p-2 w-full rounded-md outline-none ring-2 ring-inset bg-indigo-600 text-white leading-6 hover:bg-indigo-500 focus:border-blue-700 focus:ring-2 focus:ring-blue-300 ">Sign Un</button>
                         </div>
                     </form>
                 </div>
                 <div className="mt-10 mx-auto">
-                    <span>Not a member? <Link to='/register'  class="font-semibold text-indigo-600 hover:text-indigo-500">Sign Up</Link></span>
+                    <span>Already have an account? <Link to='/login'  class="font-semibold text-indigo-600 hover:text-indigo-500">Sign In</Link></span>
                 </div>
         </div>
     )
 }
 
-export default Login;
+export default Register;
